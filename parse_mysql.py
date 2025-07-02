bdbee5cf38076ac40da1035288e09eeb6dc87b6c
Optionalless <agnisdots@mail.ru>
2025-07-02 18:16:29 +0300

    Added file for parsing MySQL

---
+++ parse_mysql.py
@@ -0,0 +1,142 @@
+import xml.etree.ElementTree as Xml
+import re
+
+
+# find from result-7: DBMS, version
+def find_dmbs_upd(data, files_cur_path) -> None:
+    pattern = r"^.*(?<!\d)7\.xml$"
+    path = ""
+
+    for i in files_cur_path:
+        path = re.match(pattern, i)
+        if path is None:
+            continue
+        else:
+            path = path.group()
+            break
+
+    if path == "":
+        return None
+
+    tree = Xml.parse(path)
+    root = tree.getroot()
+
+    field = root.find("record/field")
+    value = field.get("value")
+    data["db_info"]["DBMS"] = "MySQL" if value[-3:] == "log" else "MariaDB"
+    data["db_info"]["db_upd"] = value[:-4]
+
+    return None
+
+
+# find from result-5: name, size current
+def find_name_size(data, files_cur_path) -> None:
+    pattern = r"^.*(?<!\d)5\.xml$"
+    path = ""
+
+    for i in files_cur_path:
+        path = re.match(pattern, i)
+        if path is None:
+            continue
+        else:
+            path = path.group()
+            break
+
+    if path == "":
+        return None
+
+    tree = Xml.parse(path)
+    root = tree.getroot()
+    field = root.findall("record/field")
+
+    for i in field:
+        name, value = i.get("name"), i.get("value")
+
+        if name == "database_name":
+            data["db_info"]["db_name"] = value
+        elif name == "database_size":
+            data["db_size"]["db_size_current"] = value.split(".")[0]
+
+    data["db_size"]["db_size_max"] = "Unlimited"
+
+    return None
+
+
+# find from result-1: total events
+def find_total_events(data, files_cur_path) -> None:
+    pattern = r"^.*(?<!\d)1\.xml$"
+    path = ""
+
+    for i in files_cur_path:
+        path = re.match(pattern, i)
+        if path is None:
+            continue
+        else:
+            path = path.group()
+            break
+
+    if path == "":
+        return None
+
+    tree = Xml.parse(path)
+    root = tree.getroot()
+    field = root.find("record/field").get("value")
+    data["db_total_events"] = field
+    return None
+
+
+# find from result-2: events counts
+def find_tables_events_counts(data, files_cur_path) -> None:
+    pattern = r"^.*(?<!\d)2\.xml$"
+    path = ""
+
+    for i in files_cur_path:
+        path = re.match(pattern, i)
+        if path is None:
+            continue
+        else:
+            path = path.group()
+            break
+
+    if path == "":
+        return None
+
+    tree = Xml.parse(path)
+    root = tree.getroot()
+    field = root.findall("record/field")
+    parse = {}
+    event_name, event_count = "", ""
+
+    for i in field:
+        i, ii = i.get("name"), i.get("value")
+        if i == "MAX(event_type_display_name)":
+            event_name = ii
+        elif i == "cnt":
+            event_count = ii
+            parse[event_name] = int(event_count)
+
+    data["db_events_counts"] = dict(sorted(parse.items(), key=lambda item: item[1], reverse=True))
+
+
+def find_tables_sizes(data) -> None:
+    try:
+        tree = Xml.parse("resul.xml")
+    except FileNotFoundError:
+        return None
+
+    root = tree.getroot()
+    field = root.findall("record/field")
+    parse = {}
+    name, size = "", ""
+
+    for i in field:
+        i, ii = i.get("name"), i.get("value")
+        if i == "Name":
+            name = ii
+        elif i == "reserved":
+            size = ii
+            parse[name] = size
+
+    data["db_tables_sizes"] = dict(sorted(parse.items(), key=lambda item: item[1], reverse=True))
+
+    return None
