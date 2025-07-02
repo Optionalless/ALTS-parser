cb17ac35da36633503604479266f0ddeadbc357f
Optionalless <agnisdots@mail.ru>
2025-07-02 18:16:17 +0300

    Added file for parsing MSSQL

--- parse_mssql.py
+++ parse_mssql.py
@@ -1,30 +1,29 @@
 import xml.etree.ElementTree as Xml
-
-data = {
-    "db_info": {
-        "DBMS": "DBMS",  # result-11
-        "db_upd": "db_upd",  # result-11
-        "db_name": "db_name",  # result-7
-        "db_owner": "db_owner",  # result-7
-        "db_created": "db_created",  # result-7
-    },
-    "db_size": {
-        "db_size_max": "db_size_max",  # result-8
-        "db_size_current": "db_size"  # result-7
-    },
-    "db_tables_sizes": {},
-    "db_total_events": "db_total_events"  # result-3
-}
+import re
 
 
 # find from result-11: DBMS, db_upd
-def find_dmbs_upd() -> None:
-    tree = Xml.parse('all_tables_sizes/result-11.xml')
+def find_dmbs_upd(data, files_cur_path) -> None:
+    pattern = r"^.*(?<!\d)11\.xml$"
+    path = ""
+
+    for i in files_cur_path:
+        path = re.match(pattern, i)
+        if path is None:
+            continue
+        else:
+            path = path.group()
+            break
+
+    if path == "":
+        return None
+
+    tree = Xml.parse(path)
     root = tree.getroot()
 
     field = root.find("record/field")
     field = field.get("value").split(" ")
-    name, updates = " ".join(field[0:3]), " ".join(field[4:6])
+    name, updates = " ".join(field[0:4]), " ".join(field[4:6])
     data["db_info"]["DBMS"] = name
     data["db_info"]["db_upd"] = updates
 
@@ -32,8 +31,22 @@ def find_dmbs_upd() -> None:
 
 
 # find from result-7: name, size current, owner, created
-def find_name_owner_created_size() -> None:
-    tree = Xml.parse("all_tables_sizes/result-7.xml")
+def find_name_owner_created_size(data, files_cur_path) -> None:
+    pattern = r"^.*(?<!\d)7\.xml$"
+    path = ""
+
+    for i in files_cur_path:
+        path = re.match(pattern, i)
+        if path is None:
+            continue
+        else:
+            path = path.group()
+            break
+
+    if path == "":
+        return None
+
+    tree = Xml.parse(path)
     root = tree.getroot()
     field = root.findall("record/field")
 
@@ -52,8 +65,22 @@ def find_name_owner_created_size() -> None:
 
 
 # find from result-8: size max
-def find_maxsize() -> None:
-    tree = Xml.parse("all_tables_sizes/result-8.xml")
+def find_maxsize(data, files_cur_path) -> None:
+    pattern = r"^.*(?<!\d)8\.xml$"
+    path = ""
+
+    for i in files_cur_path:
+        path = re.match(pattern, i)
+        if path is None:
+            continue
+        else:
+            path = path.group()
+            break
+
+    if path == "":
+        return None
+
+    tree = Xml.parse(path)
     root = tree.getroot()
     field = root.findall("record/field")
     for i in field:
@@ -65,8 +92,22 @@ def find_maxsize() -> None:
 
 
 # find from result-3: total events
-def find_total_events() -> None:
-    tree = Xml.parse("all_tables_sizes/result-3.xml")
+def find_total_events(data, files_cur_path) -> None:
+    pattern = r"^.*(?<!\d)3\.xml$"
+    path = ""
+
+    for i in files_cur_path:
+        path = re.match(pattern, i)
+        if path is None:
+            continue
+        else:
+            path = path.group()
+            break
+
+    if path == "":
+        return None
+
+    tree = Xml.parse(path)
     root = tree.getroot()
     field = root.find("record/field").get("value")
     data["db_total_events"] = field
@@ -74,8 +115,22 @@ def find_total_events() -> None:
 
 
 # find from result-2: tables sizes
-def find_tables_sizes() -> None:
-    tree = Xml.parse("all_tables_sizes/result-2.xml")
+def find_tables_sizes(data, files_cur_path) -> None:
+    pattern = r"^.*(?<!\d)2\.xml$"
+    path = ""
+
+    for i in files_cur_path:
+        path = re.match(pattern, i)
+        if path is None:
+            continue
+        else:
+            path = path.group()
+            break
+
+    if path == "":
+        return None
+
+    tree = Xml.parse(path)
     root = tree.getroot()
     field = root.findall("record/field")
     parse = {}
@@ -94,5 +149,36 @@ def find_tables_sizes() -> None:
     return None
 
 
-find_tables_sizes()
-print(data)
+# find from result-4: events counts
+def find_tables_events_counts(data, files_cur_path) -> None:
+    pattern = r"^.*(?<!\d)4\.xml$"
+    path = ""
+
+    for i in files_cur_path:
+        path = re.match(pattern, i)
+        if path is None:
+            continue
+        else:
+            path = path.group()
+            break
+
+    if path == "":
+        return None
+
+    tree = Xml.parse(path)
+    root = tree.getroot()
+    field = root.findall("record/field")
+    parse = {}
+    event_name, event_count = "", ""
+
+    for i in field:
+        i, ii = i.get("name"), i.get("value")
+        if i == "":
+            event_name = ii
+        elif i == "cnt":
+            event_count = ii
+            parse[event_name] = int(event_count)
+
+    data["db_events_counts"] = dict(sorted(parse.items(), key=lambda item: item[1], reverse=True))
+
+    return None
