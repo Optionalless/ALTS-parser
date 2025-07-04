22419c67c2acc476fbb198d3eeecf15c4f681c19
Optionalless <agnisdots@mail.ru>
2025-07-04 20:10:39 +0300

    transferred to the class

--- parse_mssql.py
+++ parse_mssql.py
@@ -2,183 +2,177 @@ import xml.etree.ElementTree as Xml
 import re
 
 
-# find from result-11: DBMS, db_upd
-def find_dmbs_upd(data, files_cur_path) -> None:
-    pattern = r"^.*(?<!\d)11\.xml$"
-    path = ""
+class MSSQL:
 
-    for i in files_cur_path:
-        path = re.match(pattern, i)
-        if path is None:
-            continue
-        else:
-            path = path.group()
-            break
+    data = {
+        "db_info": {
+            "DBMS": "DBMS",  # result-11
+            "db_upd": "Undefined",  # result-11
+            "db_name": "db_name",  # result-7
+            "db_owner": "Undefined",  # result-7
+            "db_created": "Undefined",  # result-7
+        },
+        "db_size": {
+            "db_size_max": "db_size_max",  # result-8
+            "db_size_current": "db_size"  # result-7
+        },
+        "db_tables_sizes": {},
+        "db_events_counts": {},
+        "db_total_events": "db_total_events"  # result-3
+    }
 
-    if path == "":
+    # search .xml-files in current path
+    @staticmethod
+    def find_files_in_cur_path(pattern, files_cur_path) -> str:
+        for i in files_cur_path:
+            path = re.match(pattern, i)
+            if path is None:
+                continue
+            else:
+                path = path.group()
+                return path
+
+    # find from result-11: DBMS, db_upd
+    def find_dmbs_upd(self, files_cur_path) -> None:
+        pattern = r"^.*(?<!\d)11\.xml$"
+
+        path = self.find_files_in_cur_path(pattern, files_cur_path)
+
+        if path == "":
+            return None
+
+        tree = Xml.parse(path)
+        root = tree.getroot()
+        field = root.find("record/field")
+        field = field.get("value").split(" ")
+        name, updates = " ".join(field[0:4]), " ".join(field[4:6])
+        self.data["db_info"]["DBMS"] = name
+        self.data["db_info"]["db_upd"] = updates
+
         return None
 
-    tree = Xml.parse(path)
-    root = tree.getroot()
+    # find from result-7: name, size current, owner, created
+    def find_name_owner_created_size(self, files_cur_path) -> None:
+        pattern = r"^.*(?<!\d)7\.xml$"
 
-    field = root.find("record/field")
-    field = field.get("value").split(" ")
-    name, updates = " ".join(field[0:4]), " ".join(field[4:6])
-    data["db_info"]["DBMS"] = name
-    data["db_info"]["db_upd"] = updates
+        path = self.find_files_in_cur_path(pattern, files_cur_path)
 
-    return None
+        if path == "":
+            return None
 
+        tree = Xml.parse(path)
+        root = tree.getroot()
+        field = root.findall("record/field")
 
-# find from result-7: name, size current, owner, created
-def find_name_owner_created_size(data, files_cur_path) -> None:
-    pattern = r"^.*(?<!\d)7\.xml$"
-    path = ""
+        for i in field:
+            name, value = i.get("name"), i.get("value")
+            if name == "name":
+                self.data["db_info"]["db_name"] = value
+            if name == "db_size":
+                self.data["db_size"]["db_size_current"] = value.strip(" ")
+            if name == "owner":
+                self.data["db_info"]["db_owner"] = value
+            if name == "created":
+                self.data["db_info"]["db_created"] = value
 
-    for i in files_cur_path:
-        path = re.match(pattern, i)
-        if path is None:
-            continue
-        else:
-            path = path.group()
-            break
-
-    if path == "":
         return None
 
-    tree = Xml.parse(path)
-    root = tree.getroot()
-    field = root.findall("record/field")
+    # find from result-8: size max
+    def find_maxsize(self, files_cur_path) -> None:
+        pattern = r"^.*(?<!\d)8\.xml$"
 
-    for i in field:
-        name, value = i.get("name"), i.get("value")
-        if name == "name":
-            data["db_info"]["db_name"] = value
-        if name == "db_size":
-            data["db_size"]["db_size_current"] = value.strip(" ")
-        if name == "owner":
-            data["db_info"]["db_owner"] = value
-        if name == "created":
-            data["db_info"]["db_created"] = value
+        path = self.find_files_in_cur_path(pattern, files_cur_path)
 
-    return None
+        if path == "":
+            return None
 
+        tree = Xml.parse(path)
+        root = tree.getroot()
+        field = root.findall("record/field")
+        for i in field:
+            if "maxsize" == i.get("name"):
+                self.data["db_size"]["db_size_max"] = i.get("value")
+                break
 
-# find from result-8: size max
-def find_maxsize(data, files_cur_path) -> None:
-    pattern = r"^.*(?<!\d)8\.xml$"
-    path = ""
-
-    for i in files_cur_path:
-        path = re.match(pattern, i)
-        if path is None:
-            continue
-        else:
-            path = path.group()
-            break
-
-    if path == "":
         return None
 
-    tree = Xml.parse(path)
-    root = tree.getroot()
-    field = root.findall("record/field")
-    for i in field:
-        if "maxsize" == i.get("name"):
-            data["db_size"]["db_size_max"] = i.get("value")
-            break
+    # find from result-3: total events
+    def find_total_events(self, files_cur_path) -> None:
+        pattern = r"^.*(?<!\d)3\.xml$"
 
-    return None
+        path = self.find_files_in_cur_path(pattern, files_cur_path)
 
+        if path == "":
+            return None
 
-# find from result-3: total events
-def find_total_events(data, files_cur_path) -> None:
-    pattern = r"^.*(?<!\d)3\.xml$"
-    path = ""
+        tree = Xml.parse(path)
+        root = tree.getroot()
+        field = root.find("record/field").get("value")
+        self.data["db_total_events"] = field
 
-    for i in files_cur_path:
-        path = re.match(pattern, i)
-        if path is None:
-            continue
-        else:
-            path = path.group()
-            break
-
-    if path == "":
         return None
 
-    tree = Xml.parse(path)
-    root = tree.getroot()
-    field = root.find("record/field").get("value")
-    data["db_total_events"] = field
-    return None
+    # find from result-2: tables sizes
+    def find_tables_sizes(self, files_cur_path) -> None:
+        pattern = r"^.*(?<!\d)2\.xml$"
 
+        path = self.find_files_in_cur_path(pattern, files_cur_path)
 
-# find from result-2: tables sizes
-def find_tables_sizes(data, files_cur_path) -> None:
-    pattern = r"^.*(?<!\d)2\.xml$"
-    path = ""
+        if path == "":
+            return None
 
-    for i in files_cur_path:
-        path = re.match(pattern, i)
-        if path is None:
-            continue
-        else:
-            path = path.group()
-            break
+        tree = Xml.parse(path)
+        root = tree.getroot()
+        field = root.findall("record/field")
+        parse = {}
+        name, size = "", ""
 
-    if path == "":
+        for i in field:
+            i, ii = i.get("name"), i.get("value")
+            if i == "Name":
+                name = ii
+            elif i == "reserved":
+                size = int(ii.split(" ")[0]) // 1024  # Transform KB to MB
+                parse[name] = size
+
+        self.data["db_tables_sizes"] = dict(sorted(parse.items(), key=lambda item: item[1], reverse=True))
+
         return None
 
-    tree = Xml.parse(path)
-    root = tree.getroot()
-    field = root.findall("record/field")
-    parse = {}
-    name, size = "", ""
+    # find from result-4: events counts
+    def find_tables_events_counts(self, files_cur_path) -> None:
+        pattern = r"^.*(?<!\d)4\.xml$"
 
-    for i in field:
-        i, ii = i.get("name"), i.get("value")
-        if i == "Name":
-            name = ii
-        elif i == "reserved":
-            size = int(ii.split(" ")[0]) // 1024
-            parse[name] = size
+        path = self.find_files_in_cur_path(pattern, files_cur_path)
 
-    data["db_tables_sizes"] = dict(sorted(parse.items(), key=lambda item: item[1], reverse=True))
+        if path == "":
+            return None
 
-    return None
+        tree = Xml.parse(path)
+        root = tree.getroot()
+        field = root.findall("record/field")
+        parse = {}
+        event_name, event_count = "", ""
 
+        for i in field:
+            i, ii = i.get("name"), i.get("value")
+            if i == "":
+                event_name = ii
+            elif i == "cnt":
+                event_count = ii
+                parse[event_name] = int(event_count)
 
-# find from result-4: events counts
-def find_tables_events_counts(data, files_cur_path) -> None:
-    pattern = r"^.*(?<!\d)4\.xml$"
-    path = ""
+        self.data["db_events_counts"] = dict(sorted(parse.items(), key=lambda item: item[1], reverse=True))
 
-    for i in files_cur_path:
-        path = re.match(pattern, i)
-        if path is None:
-            continue
-        else:
-            path = path.group()
-            break
-
-    if path == "":
         return None
 
-    tree = Xml.parse(path)
-    root = tree.getroot()
-    field = root.findall("record/field")
-    parse = {}
-    event_name, event_count = "", ""
+    # call all funcs and return data
+    def call_all_fucs(self, listing) -> dict:
+        self.find_name_owner_created_size(listing)
+        self.find_dmbs_upd(listing)
+        self.find_maxsize(listing)
+        self.find_total_events(listing)
+        self.find_tables_sizes(listing)
+        self.find_tables_events_counts(listing)
 
-    for i in field:
-        i, ii = i.get("name"), i.get("value")
-        if i == "":
-            event_name = ii
-        elif i == "cnt":
-            event_count = ii
-            parse[event_name] = int(event_count)
-
-    data["db_events_counts"] = dict(sorted(parse.items(), key=lambda item: item[1], reverse=True))
-
-    return None
+        return self.data
