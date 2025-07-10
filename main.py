c2836832254e9669c8e7d9374ad6b2bd45e84dc8
Ayaz <76888474+Optionalless@users.noreply.github.com>
2025-07-10 09:47:13 +0300

    Init file

--- main.py
+++ main.py
@@ -4,43 +4,51 @@ import ctypes
 ctypes.windll.kernel32.SetConsoleTitleW("ALTS PARSER")
 data = call_all_funcs()
 
-print("""
-             ┏┓┓ ┏┳┓┏┓ @Optionalles          
-             ┣┫┃  ┃ ┗┓  ┏┓┏┓┏┓┏┏┓┏┓
-             ┛┗┗┛ ┻ ┗┛  ┣┛┗┻┛ ┛┗ ┛ 
-                        ┛""")  # by Optionalles
 
-print(f"""――――――――――――――――――――――――――――――――――――――――――――
-│ СУБД: {data["db_info"]["DBMS"]}
-│ Обновления/версия: {data["db_info"]["db_upd"]}
-│ Наименование: {data["db_info"]["db_name"]}
-│ Владелец: {data["db_info"]["db_owner"]}
-│ Создано: {data["db_info"]["db_created"]}
-│――――――――――――――――――――――――――――――――――――――――――――
-│ Текущий Объём: {data["db_size"]["db_size_current"]}
-│ Максимальнный: {data["db_size"]["db_size_max"]}
-│――――――――――――――――――――――――――――――――――――――――――――
-│ Общее количество событий: {data["db_total_events"]}
-―――――――――――――――――――――――――――――――――――――――――――――\n""")
+def out_data(data):
+  
+  print("""
+               ┏┓┓ ┏┳┓┏┓ @Optionalles          
+               ┣┫┃  ┃ ┗┓  ┏┓┏┓┏┓┏┏┓┏┓
+               ┛┗┗┛ ┻ ┗┛  ┣┛┗┻┛ ┛┗ ┛ 
+                          ┛""")  # by Optionalles
+  
+  print(f"""――――――――――――――――――――――――――――――――――――――――――――
+  │ СУБД: {data["db_info"]["DBMS"]}
+  │ Обновления/версия: {data["db_info"]["db_upd"]}
+  │ Наименование: {data["db_info"]["db_name"]}
+  │ Владелец: {data["db_info"]["db_owner"]}
+  │ Создано: {data["db_info"]["db_created"]}
+  │――――――――――――――――――――――――――――――――――――――――――――
+  │ Текущий Объём: {data["db_size"]["db_size_current"]}
+  │ Максимальнный: {data["db_size"]["db_size_max"]}
+  │――――――――――――――――――――――――――――――――――――――――――――
+  │ Общее количество событий: {data["db_total_events"]}
+  ―――――――――――――――――――――――――――――――――――――――――――――\n""")
+  
+  cnt = 0
+  if data["db_tables_sizes"] != {}:
+      print("           Топ-10 таблиц (MB)")
+      for i, ii in data["db_tables_sizes"].items():
+          cnt += 1
+          print(f"""―――――――――――――――――――――――――――――――――――――――――――――\n│ {i} │ {ii}""")
+          if cnt == 10:
+              print("―――――――――――――――――――――――――――――――――――――――――――――\n")
+              break
+  
+  cnt = 0
+  if data["db_events_counts"] != {}:
+      print("             Топ-10 событий")
+      for i, ii in data["db_events_counts"].items():
+          cnt += 1
+          print(f"""―――――――――――――――――――――――――――――――――――――――――――――\n│ {i} │ {ii}""")
+          if cnt == 10:
+              print("―――――――――――――――――――――――――――――――――――――――――――――")  # 45
+              break
+  
+  input("\nInput any key for exit: ")
 
-cnt = 0
-if data["db_tables_sizes"] != {}:
-    print("           Топ-10 таблиц (MB)")
-    for i, ii in data["db_tables_sizes"].items():
-        cnt += 1
-        print(f"""―――――――――――――――――――――――――――――――――――――――――――――\n│ {i} │ {ii}""")
-        if cnt == 10:
-            print("―――――――――――――――――――――――――――――――――――――――――――――\n")
-            break
 
-cnt = 0
-if data["db_events_counts"] != {}:
-    print("             Топ-10 событий")
-    for i, ii in data["db_events_counts"].items():
-        cnt += 1
-        print(f"""―――――――――――――――――――――――――――――――――――――――――――――\n│ {i} │ {ii}""")
-        if cnt == 10:
-            print("―――――――――――――――――――――――――――――――――――――――――――――")  # 45
-            break
-
-input("\nInput any key for exit: ")
+if __name__ == "__main__":
+  out_data(data)
+  
