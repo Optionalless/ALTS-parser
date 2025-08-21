c3b3e2dcae8da6fd64f3b7ee3f8a88f27cd69ae2
Optionalless <agnisdots@mail.ru>
2025-08-21 18:55:14 +0300

    Changes

--- dbms_checker.py
+++ dbms_checker.py
@@ -5,34 +5,62 @@ from parse_mssql import MSSQL
 from parse_mysql import MySQL
 from parse_postgresql import PostgreSQL
 
+
 mssql = MSSQL()
 mysql = MySQL()
-postgresql = PostgreSQL()
+postgres = PostgreSQL()
+directory = ".\\"
+
 
 # Finding ALTS_files in current directory and adding this in [listing]
-dir_files = os.listdir()
-pattern = r"^.*\d{1}\.xml$"
-listing = []
-for i in dir_files:
-    listing = [f for f in dir_files if re.match(pattern, f)]
+def find_alts_files_in_curpath():
+    # check curpath
+    dir_files = os.listdir()
+    MSSQL.directory = str(os.getcwd() + "\\")
+    MySQL.directory = str(os.getcwd() + "\\")
+    PostgreSQL.directory = str(os.getcwd() + "\\")
+    pattern = r"^.*\d{1}\.xml$"
+    list_of_alts_files = []
+    for _ in dir_files:
+        list_of_alts_files = [f for f in dir_files if re.match(pattern, f)]
+    if len(list_of_alts_files) > 0:
+        return list_of_alts_files
+    else:
+        return None
 
 
-def check_mssql() -> str | None:
+def find_alts_files_on_path(path):
+    dir_files = os.listdir(path=path)
+    global directory
+    directory = str(path + "\\")
+    MSSQL.directory = directory
+    MySQL.directory = directory
+    PostgreSQL.directory = directory
+    pattern = r"^.*\d{1}\.xml$"
+    list_of_alts_files = []
+    for _ in dir_files:
+        list_of_alts_files = [f for f in dir_files if re.match(pattern, f)]
+    if len(list_of_alts_files) > 0:
+        return list_of_alts_files
+
+
+def check_mssql(alts_files) -> str | None:
     pattern = r"^.*(?<!\d)11\.xml$"
-    path = ""
+    result = ""
 
-    for i in listing:
-        path = re.match(pattern, i)
-        if path is None:
+    for i in alts_files:
+        match = re.match(pattern, i)
+
+        if match is None:
             continue
         else:
-            path = path.group()
+            result = match.group()
             break
 
-    if path == "":
+    if result == "":
         return "Не обнаружено результирующих файлов MSSQL"
 
-    tree = Xml.parse(path)
+    tree = Xml.parse(directory + result)
     root = tree.getroot()
 
     field = root.find("record/field")
@@ -42,22 +70,22 @@ def check_mssql() -> str | None:
     return name
 
 
-def check_mysql() -> str | None:
-    pattern = r"^.*(?<!\d)7\.xml$"
-    path = ""
+def check_mysql(alts_files) -> str | None:
 
-    for i in listing:
-        path = re.match(pattern, i)
-        if path is None:
+    pattern = r"^.*(?<!\d)7\.xml$"
+    result = ""
+
+    for i in alts_files:
+        match = re.match(pattern, i)
+        if match is None:
             continue
         else:
-            path = path.group()
+            result = match.group()
             break
 
-    if path == "":
+    if result == "":
         return "Не обнаружено результирующих файлов MySQL"
-
-    tree = Xml.parse(path)
+    tree = Xml.parse(directory + result)
     root = tree.getroot()
 
     field = root.find("record/field")
@@ -72,22 +100,22 @@ def check_mysql() -> str | None:
     return name
 
 
-def check_postgresql() -> str | None:
+def check_postgresql(alts_files) -> str | None:
     pattern = r"^.*(?<!\d)8\.xml$"
-    path = ""
+    result = ""
 
-    for i in listing:
-        path = re.match(pattern, i)
-        if path is None:
+    for i in alts_files:
+        match = re.match(pattern, i)
+        if match is None:
             continue
         else:
-            path = path.group()
+            result = match.group()
             break
 
-    if path == "":
+    if result == "":
         return "Не обнаружено результирующих файлов PostgreSQL"
 
-    tree = Xml.parse(path)
+    tree = Xml.parse(directory + result)
     root = tree.getroot()
 
     field = root.find("record/field")
@@ -97,20 +125,28 @@ def check_postgresql() -> str | None:
     return name
 
 
-def call_all_funcs():
-    result_mysql = check_mysql()
-    if result_mysql == "MySQL" or result_mysql == "MariaDB":
-        # MySQL parser
-        data = mysql.call_all_fucs(listing)
-        return data
-    else:
-        result_mssql = check_mssql()
-        if result_mssql[0:9] == "Microsoft":
-            # MSSQL parser
-            data = mssql.call_all_fucs(listing)
+def checker(list_of_alts_files):
+    if list_of_alts_files is not None:
+        result_mysql = check_mysql(list_of_alts_files)
+        if result_mysql == "MySQL" or result_mysql == "MariaDB":
+            # MySQL parser
+            data = mysql.call_all_fucs(list_of_alts_files)
             return data
         else:
-            result_postgresql = check_postgresql()
-            if result_postgresql[0:10] == "PostgreSQL":
-                data = postgresql.call_all_fucs(listing)
+            result_mssql = check_mssql(list_of_alts_files)
+            if result_mssql[0:9] == "Microsoft":
+                # MSSQL parser
+                data = mssql.call_all_fucs(list_of_alts_files)
                 return data
+            else:
+                result_postgresql = check_postgresql(list_of_alts_files)
+                if result_postgresql[0:10] == "PostgreSQL":
+                    data = postgres.call_all_fucs(list_of_alts_files)
+                    return data
+    else:
+        data = MSSQL.data
+        return data
+
+
+if "__main__" == __name__:
+    checker(find_alts_files_in_curpath())
